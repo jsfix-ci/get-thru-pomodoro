907ed6927cf9657ebfb3e52a899278e12a10f506
Rohan-154 <86555869+Rohan-154@users.noreply.github.com>
2022-04-05 13:41:13 +0530

    feat : added home page

---
+++ src/Components/HomeBody/homeBody.jsx
@@ -0,0 +1,48 @@
+import heroImg from "../../Assets/hero-img.jpg";
+import ashneerMeme from "../../Assets/ashneer-meme.jpg";
+import "../HomeBody/homeBody.css";
+import { Link } from "react-router-dom";
+const Body = () => {
+  return (
+    <>
+      <div className="wrap-side-content">
+        <div className="caption-main">
+          <img
+            src={ashneerMeme}
+            alt="meme"
+            className="res-img-hero res-hero-meme"
+          />
+          <h2>If you don't want your inner self to read above image, </h2>
+          <h2>
+            Then be <span className="underline"> focused </span>,{" "}
+            <span className="underline">map your tasks</span> and adopt
+            <span className="underline"> POMODORO technique </span>.
+          </h2>
+          <h3 clasName="h3-tags">
+            {" "}
+            <strong className="underline">GetThru</strong> is a one stop todo or
+            pomodoro app to complete you daily tasks and help you to adopt new
+            and good habits.{" "}
+          </h3>
+
+          <Link to="/todos">
+            <button className="btn-com btn-primary-outline">
+              {" "}
+              Get, Set, Go{" "}
+            </button>
+          </Link>
+        </div>
+
+        <div>
+          <img
+            src={heroImg}
+            alt="main-head-img"
+            className="res-img-hero res-img-main"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { Body };
---
+++ src/Routes/homePage.jsx
@@ -0,0 +1,15 @@
+import { Footer } from "../Components/Footer/footer";
+import { Body } from "../Components/HomeBody/homeBody";
+
+
+const HomePage = () => {
+ 
+  return (
+    <>
+     <Body/>
+     <Footer/>
+    </>
+  );
+};
+
+export { HomePage };
